ec2233821a54c611ea80a1fa5bf593a57832f2ae
rcordi <64288591+rcordi@users.noreply.github.com>
2026-02-17 14:12:41 -0500

    Update footer copyright text format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="relative py-6 text-center text-gray-400 bg-transparent">
       <p className="text-gray-400 text-sm">
-        © {new Date().getFullYear()} Cordibytes · Built with 💜 + 🔵
+        © {new Date().getFullYear()} · Built with 💜 + 🔵
       </p>
     </footer>
   )
